a8011d87229c70932b4695f6a7478f602adf211b
Dileep <dileepbatti00@gmail.com>
2025-01-17 19:56:04 +0530

    first commit for key project

---
+++ src/main/java/com/silca/SilcaApplication.java
@@ -0,0 +1,13 @@
+package com.silca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silca/silca/SilcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silca.silca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
